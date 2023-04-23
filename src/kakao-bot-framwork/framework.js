8161c6fbc0134f2301b45ca3e0b5a2ed72ffebe0
Hyunseung Ryu <56152093+Rhseung@users.noreply.github.com>
2023-04-23 22:44:07 +0900

    .

--- src/kakao-bot-framwork/framework.js
+++ src/kakao-bot-framwork/framework.js
@@ -8,7 +8,7 @@ const isConstructor = (f) => {
         new f();
     } catch (err) {
         if (err.message.indexOf('is not a constructor') >= 0) {
-        return false;
+            return false;
         }
     }
     return true;
@@ -32,49 +32,52 @@ const runAccordingType = (e, msg) => {
 /**
  * @object COMMANDS container  
  */
-var COMMANDS = {
-    _: [],
+var registry = {
+    commands: [],
 
-    configFunctions: {
-        canDM: (msg, value) => value == false ? (!msg.isGroupChat && !msg.isDebugRoom ? false : true) : true,
-        canGroupChat: (msg, value) => value == false ? (msg.isGroupChat ? false : true) : true,
-        activateRooms: (msg, value) => value.includes(msg.room) && value.length != 0
+    configs: {
+        canDM: (msg, value) => value == false ? !(!msg.isGroupChat && !msg.isDebugRoom && !msg.isOpenChat) : true,
+        canOpenChat: (msg, value) => value == false ? !msg.isOpenChat : true,
+        canGroupChat: (msg, value) => value == false ? !msg.isGroupChat : true,
+        canDebugChat: (msg, value) => value == false ? !msg.isDebugRoom : true,
+        rooms: (msg, value) => value.includes(msg.room) && value.length != 0,
+        onlyDual: (msg, value) => value == true ? msg.systemUserId == 95 : true
     },
 };
 
-COMMANDS.register = function() {
-    COMMANDS._ = COMMANDS._.concat(
+registry.register = function() {
+    registry.commands = registry.commands.concat(
         (arguments[0].constructor == Array) ? arguments[0] : Array.from(arguments)
     );
 };
 
-COMMANDS.set = COMMANDS.register;
+registry.set = registry.register;
 
-COMMANDS.makeConfig = function(configname, func) {
-    COMMANDS.configFunctions[configname] = func;
+registry.make = function(configname, func) {
+    registry.configs[configname] = func;
 
-    botCommand.prototype['set' + configname[0].toUpperCase() + configname.slice(1)] = function(e) {
+    cmd.prototype[configname] = function(e) {
         this.configs[configname] = e;
     
         return this;
     };
 };
 
-COMMANDS.isSatisfied = function(msg, configs) {
+registry.isSatisfied = function(msg, configs) {
     for (configName in configs) {
-        if (COMMANDS.configFunctions[configName](msg, configs[configName]) == false) {
+        if (registry.configs[configName](msg, configs[configName]) == false) {
             return false;
         }
     }
     return true;
 };
 
-COMMANDS.execute = function(msg) {
+registry.execute = function(msg) {
     var arguments = msg.args;
 
     // 첫 명령어 검색
-    var command = COMMANDS._.find(command =>
-        command(msg.command).name.string.includes(msg.command) && COMMANDS.isSatisfied(msg, command(msg.command).configs)
+    var command = registry.commands.find(command =>
+        command(msg.command).name.string.includes(msg.command) && registry.isSatisfied(msg, command(msg.command).configs)
     );
     
     // 검색 실패 시, undefined 반환
@@ -93,7 +96,7 @@ COMMANDS.execute = function(msg) {
             if (command.name.condition == null) return true;
 
             // config 검사
-            if (COMMANDS.isSatisfied(msg, subcmd.configs) == false) return false;
+            if (registry.isSatisfied(msg, subcmd.configs) == false) return false;
             
             // matchf가 생성자 함수면 타입 변환
             if (isConstructor(command.name.condition)) {
@@ -141,7 +144,7 @@ COMMANDS.execute = function(msg) {
  * `subcommand` command's argument  
  * `endcommand` command's end command, no more arguments
  */
-function botCommand() {
+function cmd() {
     this.name = {
         string : Array.from(arguments),
         condition : null
@@ -159,27 +162,32 @@ function botCommand() {
  * @param {String[]} aliases
  * @example new botCommand().setName('name1', 'name2', 'name3', ... etc);
  */
-botCommand.prototype.setName = function() {
+cmd.prototype.setName = function() {
     this.name.string = Array.from(arguments);
 
     return this;
-},
+};
+
+cmd.prototype.name = cmd.prototype.setName;
 
 /**
  * @param {String} description 
  * @example new botCommand().setDescription('description')
  */
-botCommand.prototype.setDescription = function(description) {
+cmd.prototype.setDescription = function(description) {
     this.description = description;
 
     return this;
-},
+};
+
+cmd.prototype.description = cmd.prototype.setDescription;
+cmd.prototype.desc = cmd.prototype.setDescription;
 
 /**
  * @param {Object} configs 
  * @example new botCommand().setConfigs({ config1: true, config2: false, ... etc })
  */
-botCommand.prototype.setConfigs = function(configs) {
+cmd.prototype.setConfigs = function(configs) {
     for (let i in configs) {
         this.configs[i] = configs[i];
     }
@@ -187,7 +195,7 @@ botCommand.prototype.setConfigs = function(configs) {
     return this;
 },
 
-botCommand.prototype.setOptions = botCommand.prototype.setConfigs;
+cmd.prototype.configs = cmd.prototype.setConfigs;
 
 /**
  * @param {Function} match 
@@ -195,7 +203,7 @@ botCommand.prototype.setOptions = botCommand.prototype.setConfigs;
  * @example new botCommand().addArgument(String, argname => new botCommand().addArgument(... etc) ... etc)
  * @example new botCommand().addArgument(msg => msg.author.name.startsWith("@"), username => new botCommand().addArgument(... etc) ... etc)
  */
-botCommand.prototype.addArgument = function(match, subcommand) {
+cmd.prototype.addArgument = function(match, subcommand) {
     this.name.condition = match;
 
     // subcommand().description = subcommand().description || this.description;
@@ -205,14 +213,14 @@ botCommand.prototype.addArgument = function(match, subcommand) {
     return this;
 },
 
-botCommand.prototype.arg = botCommand.prototype.addArgument;
+cmd.prototype.arg = cmd.prototype.addArgument;
 
 /**
  * @param {Function} match 
  * @param {botCommand} endcommand 
  * @example new botCommand().addArguments(Number, numbers => new botCommand().run(... etc) ... etc)
  */
-botCommand.prototype.addArguments = function(match, endcommand) {
+cmd.prototype.addArguments = function(match, endcommand) {
     this.name.condition = match;
     this.isGrouped = true;
 
@@ -223,7 +231,7 @@ botCommand.prototype.addArguments = function(match, endcommand) {
     return this;
 },
 
-botCommand.prototype.args = botCommand.prototype.addArguments;
+cmd.prototype.args = cmd.prototype.addArguments;
 
 /**
  * @param {String | Number | any[] | Function} e 
@@ -245,17 +253,17 @@ botCommand.prototype.args = botCommand.prototype.addArguments;
  * @example new botCommand().run([1, [2, [msg => msg.reply(3), "4"]], 5])
  * @example new botCommand().run(msg => msg.reply("hello world"))
  */
-botCommand.prototype.run = function(e) {
+cmd.prototype.run = function(e) {
     this.runcode = e;
     // todo run(a, b, c, d, e, f, ...)
 
     return this;
 }
 
-botCommand.prototype.do = botCommand.prototype.run;
+cmd.prototype.do = cmd.prototype.run;
 
-for (let config in COMMANDS.configFunctions) {
-    botCommand.prototype['set' + config[0].toUpperCase() + config.slice(1)] = function(e) {
+for (let config in registry.configs) {
+    cmd.prototype['set' + config[0].toUpperCase() + config.slice(1)] = function(e) {
         this.configs[config] = e;
     
         return this;
@@ -263,31 +271,31 @@ for (let config in COMMANDS.configFunctions) {
 }
 
 module.exports = {
-    botCommand: botCommand,
-    COMMANDS: COMMANDS
+    cmd: cmd,
+    registry: registry
 };
 
-////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////
 
-COMMANDS.makeConfig('allowLevel', (msg, value) => (msg.level || 0) >= value);
-COMMANDS.register(
-    add => new botCommand('add', 'plus')
-        .setActivateRooms(['dev'])
-        .setCanDM(false)
-        .setAllowLevel(4)
+// registry.make('allowLevel', (msg, value) => (msg.level || 0) >= value);
+// registry.register(
+//     add => new cmd('add', 'plus')
+//         .rooms(['dev'])
+//         .canDM(false)
+//         .allowLevel(4)
 
-        .args(Number, numbers => new botCommand()
-            .do(msg =>
-                numbers.reduce((acc, curr) => acc + curr)
-            )
-        )
-);
+//         .args(Number, numbers => new cmd()
+//             .do(msg =>
+//                 numbers.reduce((acc, curr) => acc + curr)
+//             )
+//         )
+// );
 
-console.log(COMMANDS.execute({
-    args: ['1', '2', '3'],
-    command: 'add',
-    room: 'dev',
-    isGroupChat: false,
-    isDebugRoom: true,
-    level: 4
-}));
+// console.log(registry.execute({
+//     args: ['1', '2', '3'],
+//     command: 'add',
+//     room: 'dev',
+//     isGroupChat: false,
+//     isDebugRoom: true,
+//     level: 4
+// }));
